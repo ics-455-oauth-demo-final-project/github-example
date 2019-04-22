ea49272a347832c0ac127e807ad1d6dd764762f9
Jon Bach <bach4@hawaii.edu>
2019-04-21 18:17:22 -1000

    Update index.php

--- index.php
+++ index.php
@@ -149,7 +149,6 @@ MouseBubbles();
     <ul>
       <li><a href="/demo/githubOauthDemo.php">Github Oauth Data Example</a></li>
       <li><a href="/demo/facebookOauthDemo.php">Facebook Oauth Data Example</a></li>
-      <li><a href="/demo/googleOauthDemo.php">Facebook Oauth Data Example</a></li>
     </ul>
 </div>
 </body>
